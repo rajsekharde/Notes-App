416fd13c9e994f9987252fc1a6bb5aa12ae39822
RsD1405 <rajsekharde612@gmail.com>
2025-11-30 03:16:06 +0530

    Added secret-key, token variables to config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,6 +3,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
 
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINS: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+
     class Config:
         env_file = ".env"
 
